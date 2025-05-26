0066b159caff05c82c067445cbca281db050af4b
BlueFyre22 <bluefyr22@gmail.com>
2025-05-25 20:22:17 -0600

    basic layout done

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const iceCream = [
+  { name: "Mint", price: 1.25, quantity: 0, type: "scoop" },
+  { name: "Vanilla", price: 1, quantity: 0, type: "scoop" },
+  { name: "Strawberry", price: 1.25, quantity: 0, type: "scoop" },
+  { name: "Sprinkles", price: 0.25, quantity: 0, type: "topping" },
+  { name: "Chocolate Chips", price: 0.25, quantity: 0, type: "topping" },
+  { name: "Cookie Chunks", price: 0.5, quantity: 0, type: "topping" },
+];
